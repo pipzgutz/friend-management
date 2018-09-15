6ac1cbd135a9d606018eb81a9c4cf03c552b74a4
Philip Mark Gutierrez <philipmarkgutierrez@gmail.com>
2018-09-15 11:04:59 +0800

    initial commit

---
+++ src/main/java/com/capgemini/friendmanagement/FriendManagementApplication.java
@@ -0,0 +1,12 @@
+package com.capgemini.friendmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FriendManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FriendManagementApplication.class, args);
+	}
+}
